a5d41602640f54af746d36f4660a433fdff2200a
Khris <khristianmarcial@gmail.com>
2019-10-21 23:49:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne,cardTwo);
-
-if (cardsInPlay.length === 2) {
-    console.log("Yes");
-   if (cardsInPlay[0] === cardsInPlay[1]) {
-       alert("You found a match!");
-   } else if (cardsInPlay[0] !== cardsInPlay[1]) {
-       alert("Sorry, try again.");
-   }
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 }
 
-console.log(cardsInPlay);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);```
+function flipCard(cardId) {
+    
+    cardsInPlay.push(cards[cardId]);
+
+    checkForMatch();
+    console.log("User flipped " + cards[cardId])
+}
+
+
+flipCard(0);
+flipCard(2);
